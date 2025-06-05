3e34c00177f8cf30716632cd62c448d7df11c27a
Pranav <pranavk24h@gmail.com>
2025-06-05 15:23:44 +0530

    configure routes

---
+++ app/companions/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CompanionSession() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CompanionSession
---
+++ app/companions/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function CompanionsLibrary() {
+  return (
+    <div>
+        Companions Library
+    </div>
+  )
+}
+
