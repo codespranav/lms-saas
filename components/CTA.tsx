c930045f5a5976c8444991de73ead4ff8d730810
Pranav <pranavk24h@gmail.com>
2025-06-15 05:24:58 +0530

    design cta

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image"
+import Link from "next/link"
+
 const CTA = () => {
   return (
-    <div>
-      CTA
+    <div className="cta-section">
+      <div className="cta-badge">Start learning your way.</div>
+      <h2 className="text-3xl font-bold">Build a Personalize Learning Companion</h2>
+      <p>Pick a name, subject, voice, & personality — and start learning through voice conversations that feel natural and fun.</p>
+      <Image src="/images/cta.svg" width={362} height={232} alt=""/>
+      <button className="btn-primary bg-[#FE5933] text-lg">
+          <Image src="/icons/plus.svg" alt="" width={12} height={12}/>
+          <Link href="/companions/new">Build New Companion</Link>
+      </button>
     </div>
   )
 }
